d1a7fac0b669e89c3e44cc59f63701e41863bd47
Justin J. Moses <justinjmoses@gmail.com>
2011-12-14 22:58:25 -0500

    Adding gmail smtp info for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,4 +57,15 @@ Subtrackr::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'baci.lindsaar.net',
+    :user_name            => 'subtrackr',
+    :password             => 'subtr4ckr!@#',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
+    
 end
