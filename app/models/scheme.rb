61732c39ae436ed2a54a0f5e9e63ec3c5fcd0cdf
Justin J. Moses <justinjmoses@gmail.com>
2011-12-25 13:29:54 +0200

    Adding scheme validation and ability to edit embedded schemes with the service

--- app/models/scheme.rb
+++ app/models/scheme.rb
@@ -1,11 +1,15 @@
 class Scheme
   include Mongoid::Document
   
-  field :name,      :type => String
-  field :amount,    :type => Float, 	default: 0
-  field :currency,  :type => String, 	default: "usd" 
-  field :frequency, :type => String, 	default: "monthly"
-  field :offset, 	type: Integer, 		default: 0 #0 means on day of creation
+  field :name,      type: String
+  field :amount,    type: Float, 	  default: 0
+  field :currency,  type: String,   default: "usd" 
+  field :frequency, type: String,   default: "monthly"
+  field :offset, 	  type: Integer,  default: 0 
 
   embedded_in :service, inverse_of: :schemes
+
+  validates_numericality_of :offset, greater_than_or_equal_to: 0, less_than_or_equal_to: 31
+  validates_inclusion_of :currency, in: ["usd"] 
+  validates_inclusion_of :frequency, in: ["weekly", "monthly", "yearly"]
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -2,10 +2,12 @@ class Service
   include Mongoid::Document
   include Mongoid::Timestamps
    
-  field :name, :type => String
-  field :url, :type => String
-  field :desc, :type => String
-  field :category, :type => String
+  field :name,      type: String
+  field :url,       type: String
+  field :desc,      type: String
+  field :category,  type: String
   
   embeds_many :schemes
+
+  accepts_nested_attributes_for :schemes
 end
