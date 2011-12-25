6534fb118936cab668f08efd12ae2aca74364dd1
Justin J. Moses <justinjmoses@gmail.com>
2011-12-25 14:07:05 +0200

    Adding scheme.amount validation and rendering of validation errors inline

--- app/models/scheme.rb
+++ app/models/scheme.rb
@@ -10,6 +10,7 @@ class Scheme
   embedded_in :service, inverse_of: :schemes
 
   validates_numericality_of :offset, greater_than_or_equal_to: 0, less_than_or_equal_to: 31
+  validates_numericality_of :amount
   validates_inclusion_of :currency, in: ["usd"] 
   validates_inclusion_of :frequency, in: ["weekly", "monthly", "yearly"]
 end
