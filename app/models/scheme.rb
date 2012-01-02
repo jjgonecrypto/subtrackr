6c12057cf8d6fc07799a6bd7cf7d028369d782c8
Justin J. Moses <justinjmoses@gmail.com>
2012-01-02 10:49:14 +0200

    Updating indendation

--- app/models/scheme.rb
+++ app/models/scheme.rb
@@ -2,10 +2,10 @@ class Scheme
   include Mongoid::Document
   
   field :name,      type: String
-  field :amount,    type: Float, 	  default: 0
+  field :amount,    type: Float,    default: 0
   field :currency,  type: String,   default: "usd" 
   field :frequency, type: String,   default: "monthly"
-  field :offset, 	  type: Integer,  default: 0 
+  field :offset,    type: Integer,  default: 0 
 
   embedded_in :service, inverse_of: :schemes
 
