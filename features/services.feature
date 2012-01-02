4b1723c93c6acfee75e76e64abbbab281beac705
Justin J. Moses <justinjmoses@gmail.com>
2012-01-02 16:25:45 +0200

    Adding feature tests for services

--- features/services.feature
+++ features/services.feature
@@ -1,10 +1,34 @@
 Feature: Services
-  In order to browse all services
+  In order to manage services
   As a user
   I want to see all services 
+  and to edit services 
+
+  @javascript
+  Scenario: Edit a Service with Schemes
+    Given I have a service named "Spotify"
+    And with a scheme named "premium" of amount "5.34"
+    And with a scheme named "ultra" of amount "23.34"
+    When I go to edit the service
+    And I delete the scheme named "premium"
+    And I submit the form
+    Then I should see "ultra" 
+    And I should not see "premium"
 
   Scenario: Services List
-    Given I have services titled Spotify, Netflix
+    Given I have services named Spotify, Netflix
     When I go to the list of services
     Then I should see "Spotify"
     And I should see "Netflix"
+
+  @javascript
+  Scenario: Create a Service with Schemes
+    When I go to a new service
+    And I enter in "Spotify" as a name
+    And I add a scheme with the name "premium" and an amount of "5.50"
+    And I submit the form
+    Then I should see "Spotify"
+    And I should see "premium" 
+    And I should see "$5.50"  
+
+  
--- features/step_definitions/service_steps.rb
+++ features/step_definitions/service_steps.rb
@@ -1,6 +1,6 @@
-Given /^I have services titled (.+)$/ do |titles|
-  titles.split(', ').each do |title|
-    Service.create!(title: title)
+Given /^I have services named (.+)$/ do |names|
+  names.split(', ').each do |name|
+    Service.create!(name: name)
   end
 end
 
@@ -8,6 +8,58 @@ Given /^I have no services$/ do
   Service.delete_all
 end
 
-Then /^I should have ([0-9]+) services?$/ do |count|
-  Service.count.should == count.to_i
+When /^I go to the list of services$/ do
+  visit services_path
 end
+
+Then /^I should see "([^\"]*)"$/ do |arg1|
+  page.should have_content(arg1)
+end
+
+Then /^I should have ([0-9]+) services?$/ do |count|
+  Service.count.should eq(count.to_i)
+end
+
+When /^I go to a new service$/ do
+  visit new_service_path
+end
+
+When /^I enter in "([^\"]*)" as a name$/ do |arg1|
+  fill_in("Name", with: arg1)
+end
+
+When /^I add a scheme with the name "([^\"]*)" and an amount of "([^\"]*)"$/ do |arg1, arg2|
+  click_link("Add scheme")  
+  within("#innerSchemes") do
+    fill_in("Name", with: arg1)
+    fill_in("Amount", with: arg2)
+  end  
+end
+
+When /^I submit the form$/ do
+  find(:xpath, "//input[@type='submit']").click
+end
+
+Given /^I have a service named "([^\"]*)"$/ do |arg1|
+  Service.create!(name: arg1)
+end
+
+Given /^with a scheme named "([^"]*)" of amount "([^"]*)"$/ do |arg1, arg2|
+  service = Service.first
+  service.schemes.create!(name: arg1, amount: arg2)
+end
+
+When /^I go to edit the service$/ do
+  visit edit_service_path(Service.first)
+end
+
+When /^I delete the scheme named "([^\"]*)"$/ do |arg1|
+  within("#innerSchemes") do 
+    click_link("Del")
+  end
+end
+
+Then /^I should not see "([^\"]*)"$/ do |arg1|
+  page.should_not have_content(arg1)
+end
+
